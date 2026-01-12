dd0adfbd78af0201ab7280f4b23d97981754e588
anjiattri <attri2707@gmail.com>
2026-01-12 20:46:05 +0530

    star

--- starpattern6.js
+++ starpattern6.js
@@ -1,7 +1,7 @@
 function starPattern6(n) {
   for (let i = 0; i < n; i++) {
     let row = "";
-    for (let k = i; k < n - 1; k++) {
+    for (let k = 0; k < n - i; k++) {
       row += " ";
     }
     for (let j = 0; j <= i; j++) {
@@ -11,4 +11,17 @@ function starPattern6(n) {
     console.log(row);
   }
 }
+// function starPattern6(n) {
+//   for (let i = n; i > 0; i--) {
+//     let row = "";
+//     for (let k = 0; k < i - 1; k++) {
+//       row += " ";
+//     }
+//     for (let j = 0; j <= n - i; j++) {
+//       row += "*";
+//     }
+
+//     console.log(row);
+//   }
+// }
 console.log(starPattern6(5));
