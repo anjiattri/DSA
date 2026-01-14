118766a5979cc18129a0866af069b4b11f95f682
anjiattri <attri2707@gmail.com>
2026-01-14 22:33:22 +0530

    remove duplicates

---
+++ removeduplicate.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x++;
+      nums[x] = nums[i];
+    }
+  }
+  return x + 1;
+};
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+// 5
