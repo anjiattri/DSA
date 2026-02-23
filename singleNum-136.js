d993afd521b1719577ef161742233291bed6d69e
anjiattri <attri2707@gmail.com>
2026-02-23 23:23:46 +0530

    single num

--- singleNum-136.js
+++ singleNum-136.js
@@ -18,4 +18,15 @@ var singleNumber = function (nums) {
     }
   }
 };
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var singleNumber = function (nums) {
+//     let xor=0
+//     for(let i=0;i<nums.length;i++){
+//         xor = xor ^ nums[i]
+//     }
+//     return xor
+// }
 console.log(singleNumber([4, 1, 2, 1, 2]));
